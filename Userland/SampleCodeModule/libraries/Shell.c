a5d96e8b0704a058dc9a67c57390abffdb1de558
Lautaro Gazzaneo <67807043+lgazzaneo@users.noreply.github.com>
2022-06-05 15:54:17 -0300

    Update Shell with Help command

--- Userland/SampleCodeModule/libraries/Shell.c
+++ Userland/SampleCodeModule/libraries/Shell.c
@@ -40,3 +40,28 @@ void printTime()
     putDec(getTime(YEAR));
     putChar('\n');
 }
+
+void printHelp(){
+    char *stringout = "En la siguiente lista se muestran los posibles comandos a ser utilizados \n";
+    stringout = strcat(stringout, "incluyendo una breve descripcion de que hacen y su uso: \n");
+    print(stringout);
+
+    print("help -- muestra una lista de las funciones posibles a ser ejecutadas.\n");
+    print("\n");
+    print("divbyzero -- muestra el funcionamiento de la excepcion conocida como division por 0.\n");
+    print("\n");
+    print("codopinv -- muestra el funcionamiento de la excepcion de codigo de oper invalido.\n");
+    print("\n");
+    print("inforeg -- realiza un dump en pantalla del valor de todos los registros.\n");
+    print("\n");
+    print("printmem -- realiza un dump en pantalla de los 32bits de memoria desde la direccion recibida como argumento.\n");
+    print("\n");    
+    print("time -- imprime el dia y la hora del sistema en formato hh:mm-dd/mm/yyyy.\n");
+    print("\n");
+    print("primos -- imprime los numeros primos desde el 1 en pantalla.\n");
+    print("\n");
+    print("fibonacchi -- imprime la serie de fibonacchi empezando en el 1...\n");
+    print("\n");
+    print("com1 | com2 -- el uso del caracter | habilitara la division de la pantalla en 2 y en cada lado se ejecutara cada comando.\n");
+    print("\n");
+}
